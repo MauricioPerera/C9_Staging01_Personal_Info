6e5ce990d8714120c3e2892f6cfad581551c07f5
Rockerfeler <mauricio.perera@gmail.com>
2022-03-17 11:22:39 -0600

    Added fake data and others
    Added fake data and beginning the upgrade of inputs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,42 @@
 import InputContainer from "./InputContainer";
 import "../styles.css";
-
 export default function App() {
-  const label = "Nombre";
-  const value = "Mi Nombre";
+  const user = {
+    idUser: "1",
+    firstName: "Mauricio",
+    secondName: "Javier",
+    firstSurname: "Perera",
+    secondSurname: "Gonzalvez",
+    gender: "male",
+    email: "123@email.com",
+    avatar: "url(image)",
+    phoneNumber: "55 55555555",
+    addres: {
+      country: "Mexico",
+      citie: "Queretaro",
+      addres: "R.Bautista 14"
+    },
+    identification: {
+      nationality: "Uruguayan",
+      idOfNationality: "5555555-5"
+    },
+    passport: "D222203",
+    favorites: {
+      idFavorite1: "1",
+      idFavorite2: "2",
+      idFavorite3: "3"
+    }
+  };
+  const name = {
+    firstName: user.firstName,
+    secondName: user.secondName,
+    firstSurname: user.firstSurname,
+    secondSurname: user.secondSurname
+  };
   return (
     <div className="personalInfo">
       <h2>Información personal</h2>
-      <InputContainer label="Nombre legal" value="Mauricio Javier Perera" />
-      <InputContainer label="Sexo" value="Masculino" />
-      <InputContainer label="Numero de teléfono" value="(555)555-55-55" />
-      <InputContainer label="Identificación" value="1234567890" />
-      <InputContainer label="Dirección" value="Calle Falsa 123" />
-      <InputContainer label="Contato de emergencia" value="(555)555-55-55" />
-      <InputContainer label="Pasaporte" value="1234567890" />
+      <InputContainer label="Nombre legal" value={name} />
     </div>
   );
 }
--- src/components/InputContainer.jsx
+++ src/components/InputContainer.jsx
@@ -12,28 +12,53 @@ export default function InputContainer({ label, value }) {
   return (
     <div className="inputContainer">
       <label>{label}</label>
-      {isActive &&
-      <span onClick={handleGameClick}>
-        Editar
-      </span>
-      }
-      {!isActive &&
-      <span onClick={handleGameClick}>
-        Guardar
-      </span>
-      }
+      {isActive && <span onClick={handleGameClick}>Editar</span>}
+      {!isActive && <span onClick={handleGameClick}>Guardar</span>}
 
-      {!isActive &&
-      <TextInput
-        onChangeText={(newText) => setText(newText)}
-        defaultValue={value}
-      />
-      }
-      {isActive &&
-      <Text >
-        {text ? text : value}
-      </Text>
-      }
+      <div className="data-input">
+        {!isActive && (
+          <div className="data-input-item">
+            <TextInput
+              onChangeText={(newText) => setText(newText)}
+              defaultValue={value.firstName}
+            />
+            <sub>Primer Nombre</sub>
+          </div>
+        )}
+        {!isActive && (
+          <div className="data-input-item">
+            <TextInput
+              onChangeText={(newText) => setText(newText)}
+              defaultValue={value.secondName}
+            />
+            <sub>Segundo Nombre</sub>
+          </div>
+        )}
+        {!isActive && (
+          <div className="data-input-item">
+            <TextInput
+              onChangeText={(newText) => setText(newText)}
+              defaultValue={value.firstSurname}
+            />
+            <sub>Primer Apellido</sub>
+          </div>
+        )}
+        {!isActive && (
+          <div className="data-input-item">
+            <TextInput
+              onChangeText={(newText) => setText(newText)}
+              defaultValue={value.secondSurname}
+            />
+            <sub>Segundo Apellido</sub>
+          </div>
+        )}
+      </div>
+      <div className="data-text">
+        {isActive && <Text>{text ? text : value.firstName}</Text>}
+        {isActive && <Text>{text ? text : value.secondName}</Text>}
+        {isActive && <Text>{text ? text : value.firstSurname}</Text>}
+        {isActive && <Text>{text ? text : value.secondSurname}</Text>}
+      </div>
     </div>
   );
 }
